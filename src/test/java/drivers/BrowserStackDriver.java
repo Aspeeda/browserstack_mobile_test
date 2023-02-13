0ec265060bde6b4918bb81479afdf69620830463
DESKTOP-T1P4KAC\User <aspeeda@yandex.ru>
2023-02-13 11:10:48 +0500

    extended browserstack(need to refactor)

--- src/test/java/drivers/BrowserStackDriver.java
+++ src/test/java/drivers/BrowserStackDriver.java
@@ -12,39 +12,39 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
-//
-//public class BrowserStackDriver implements WebDriverProvider{
-//    static Config config = ConfigFactory.create(Config.class);
+
+public class BrowserStackDriver implements WebDriverProvider{
+    static Config config = ConfigFactory.create(Config.class);
 
 
-//    @SneakyThrows
-//    @Override
-//    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-//        MutableCapabilities mutableCapabilities = new MutableCapabilities();
-//        mutableCapabilities.merge(capabilities);
-//
-//        mutableCapabilities.setCapability("app", config.);
-//        mutableCapabilities.setCapability("device", config.device());
-//        mutableCapabilities.setCapability("os_version", config.osVersion());
-//
-//        // Set your access credentials
-//        mutableCapabilities.setCapability("browserstack.user", config.login());
-//        mutableCapabilities.setCapability("browserstack.key", config.password());
-//        mutableCapabilities.setCapability("project", config.projectName());
-//        mutableCapabilities.setCapability("build", config.buildName());
-//        mutableCapabilities.setCapability("name", config.testName());
-//
-//         Initialise the remote Webdriver using BrowserStack remote URL
-//         and desired capabilities defined above
-//        return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
-//    }
-//
-//    public static URL getBrowserstackUrl() {
-//        try {
-//            return new URL(config.baseUrl());
-//        } catch (MalformedURLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
+    @SneakyThrows
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.merge(capabilities);
+
+        mutableCapabilities.setCapability("app", config.);
+        mutableCapabilities.setCapability("device", config.device());
+        mutableCapabilities.setCapability("os_version", config.osVersion());
+
+        // Set your access credentials
+        mutableCapabilities.setCapability("browserstack.user", config.login());
+        mutableCapabilities.setCapability("browserstack.key", config.password());
+        mutableCapabilities.setCapability("project", config.projectName());
+        mutableCapabilities.setCapability("build", config.buildName());
+        mutableCapabilities.setCapability("name", config.testName());
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+        return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
+    }
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL(config.baseUrl());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 
